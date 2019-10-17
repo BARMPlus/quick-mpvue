be74b6e3203bbb8beb2edf445f52ab1fd079c653
ccz <chenchongzuan@baishishuju.cn>
2019-10-17 16:48:37 +0800

    :art: 1.文件目录修改 2.loading增加

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+class Loading {
+  show(title='加载中...',mask=true){
+    wx.showLoading({
+      title,
+      mask
+    });
+  }
+  hide(){
+    wx.hideLoading()
+  }
+}
+export let loading=new Loading();
